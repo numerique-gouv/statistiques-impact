947ccbee0022ea3ffa11d5a7e7812731695c30e4
Benoit Serrano <benoit.serrano10@gmail.com>
2023-08-07 12:27:59 +0200

    add all products operateur

--- src/constants.ts
+++ src/constants.ts
@@ -1,22 +1,17 @@
 type productType = { name: string; metabaseUrl?: string; repositoriesUrl: string[] };
 
 const PRODUCTS: Record<string, productType> = {
-    ANNUAIRE_DES_ENTREPRISES: {
-        name: 'annuaire-des-entreprises',
-        repositoriesUrl: ['https://github.com/etalab/annuaire-entreprises-site'],
-    },
     AGENT_CONNECT: {
         name: 'agent-connect',
         metabaseUrl: 'https://stats.agentconnect.gouv.fr',
         repositoriesUrl: [],
     },
-    API_PARTICULIER: {
-        name: 'api-particulier',
-        metabaseUrl: 'https://metabase.entreprise.api.gouv.fr',
-        repositoriesUrl: [],
+    ANNUAIRE_DES_ENTREPRISES: {
+        name: 'annuaire-des-entreprises',
+        repositoriesUrl: ['https://github.com/etalab/annuaire-entreprises-site'],
     },
-    API_ENTREPRISE: {
-        name: 'api-entreprise',
+    API_GOUV: {
+        name: 'api-gouv',
         metabaseUrl: 'https://metabase.entreprise.api.gouv.fr',
         repositoriesUrl: [],
     },
@@ -24,6 +19,14 @@ const PRODUCTS: Record<string, productType> = {
         name: 'audioconf',
         metabaseUrl: 'https://stats.audioconf.numerique.gouv.fr/',
         repositoriesUrl: ['https://github.com/betagouv/audioconf'],
+    },
+    AUTHENTIFICATION: {
+        name: 'authentification',
+        repositoriesUrl: [],
+    },
+    CODE_GOUV: {
+        name: 'code-gouv',
+        repositoriesUrl: [],
     },
     DATAPASS: {
         name: 'datapass',
@@ -33,21 +36,33 @@ const PRODUCTS: Record<string, productType> = {
         name: 'demarches-simplifiees',
         repositoriesUrl: [],
     },
+    FRANCE_CONNECT: {
+        name: 'france-connect',
+        repositoriesUrl: [],
+    },
+    HUBEE: { name: 'hubee', repositoriesUrl: [] },
+    MON_COMPTE_PRO: {
+        name: 'mon-compte-pro',
+        repositoriesUrl: ['https://github.com/betagouv/moncomptepro'],
+        metabaseUrl: 'https://stats.moncomptepro.beta.gouv.fr/',
+    },
+    OSMOSE: { name: 'osmose', repositoriesUrl: [] },
     PAD: { name: 'pad', repositoriesUrl: ['https://github.com/betagouv/pad.numerique.gouv.fr'] },
+    RESANA: { name: 'resana', repositoriesUrl: [] },
+    RIZOMO: { name: 'rizomo', repositoriesUrl: [] },
     TCHAP: {
         name: 'tchap',
         metabaseUrl: 'https://stats.tchap.incubateur.net/',
+        repositoriesUrl: [],
+    },
+    WEBCONF: {
+        name: 'webconf',
         repositoriesUrl: [],
     },
     WEBINAIRE: {
         name: 'webinaire',
         repositoriesUrl: [],
         metabaseUrl: 'https://webinaire-metabase.osc-secnum-fr1.scalingo.io/',
-    },
-    MON_COMPTE_PRO: {
-        name: 'mon-compte-pro',
-        repositoriesUrl: ['https://github.com/betagouv/moncomptepro'],
-        metabaseUrl: 'https://stats.moncomptepro.beta.gouv.fr/',
     },
 };
 
