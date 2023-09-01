4e7c3a2f5703eeca9f882b5c54984ed63fb42055
Benoit Serrano <benoit.serrano10@gmail.com>
2023-09-01 10:23:09 +0100

    fix constant typing

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,30 @@
 type productType = { name: string; metabaseUrl?: string; repositoriesUrl: string[] };
 
-const PRODUCTS: Record<string, productType> = {
+type PRODUCT_ID =
+    | 'AGENT_CONNECT'
+    | 'ANNUAIRE_DES_ENTREPRISES'
+    | 'API_PARTICULIER'
+    | 'API_ENTREPRISE'
+    | 'AUDIOCONF'
+    | 'AUTHENTIFICATION'
+    | 'CODE_GOUV'
+    | 'DATA_GOUV'
+    | 'DATAPASS'
+    | 'DEMARCHES_SIMPLIFIEES'
+    | 'DVF'
+    | 'FRANCE_CONNECT'
+    | 'HUBEE'
+    | 'MON_COMPTE_PRO'
+    | 'OOTS'
+    | 'OSMOSE'
+    | 'PAD'
+    | 'RESANA'
+    | 'RIZOMO'
+    | 'TCHAP'
+    | 'WEBCONF'
+    | 'WEBINAIRE';
+
+const PRODUCTS: Record<PRODUCT_ID, productType> = {
     AGENT_CONNECT: {
         name: 'agent-connect',
         metabaseUrl: 'https://stats.agentconnect.gouv.fr',
@@ -16,19 +40,23 @@ const PRODUCTS: Record<string, productType> = {
             'https://github.com/etalab/annuaire-entreprises-search-testing',
         ],
     },
-    API_GOUV: {
-        name: 'api-gouv',
+
+    API_PARTICULIER: {
+        name: 'api-particulier',
+        metabaseUrl: 'https://metabase.entreprise.api.gouv.fr',
+        repositoriesUrl: [],
+    },
+    API_ENTREPRISE: {
+        name: 'api-entreprise',
         metabaseUrl: 'https://metabase.entreprise.api.gouv.fr',
         repositoriesUrl: [
-            'https://github.com/betagouv/api.gouv.fr',
             'https://github.com/etalab/admin_api_entreprise',
             'https://github.com/etalab/calendrier.api.gouv.fr',
-            'https://github.com/etalab/geo.api.gouv.fr',
-            'https://github.com/etalab/api-geo',
             'https://github.com/etalab/rncs_worker_api_entreprise',
             'https://github.com/etalab/watchdoge_apientreprise',
         ],
     },
+
     AUDIOCONF: {
         name: 'audioconf',
         metabaseUrl: 'https://stats.audioconf.numerique.gouv.fr/',
