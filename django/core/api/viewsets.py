fddafc82be30df75ad107f1539d6ec467af85660
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2025-07-24 19:14:43 +0200

    (api) migrate indicators endpoint
    "migrate" indicators endpoint from Express to Django stack. See #22

--- django/core/api/viewsets.py
+++ django/core/api/viewsets.py
@@ -3,10 +3,29 @@ from core import models
 from core.api import serializers
 
 
-class ProductViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class ProductViewSet(
+    mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet
+):
     """
-    API endpoint that allows to list existing products.
+    API endpoint to list existing products.
     """
 
     queryset = models.Product.objects.all()
     serializer_class = serializers.ProductSerializer
+    lookup_field = "id"
+
+
+class IndicatorViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """
+    API endpoint to list and manage indicators of a given product.
+    """
+
+    serializer_class = serializers.IndicatorSerializer
+    queryset = models.Indicator.objects.all()
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset()
+        if product_id := self.kwargs.get("product_id"):
+            product = models.Product.objects.get(id=product_id)
+            queryset = queryset.filter(productid=product)
+        return queryset
