eb2730efc56248e5c849b9686623e21a542c13d0
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2025-08-04 20:16:12 +0200

    (api) request bearing valid api key can create / delete indicators

--- django/core/api/viewsets.py
+++ django/core/api/viewsets.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets, mixins, permissions
+from rest_framework import viewsets, mixins
 from core import models
-from core.api import serializers
-from rest_framework_api_key.permissions import HasAPIKey
+from core.api import serializers, permissions
+from django.db.utils import IntegrityError
+from django.core import exceptions
 
 
 class ProductViewSet(
@@ -16,14 +17,21 @@ class ProductViewSet(
     lookup_field = "slug"
 
 
-class IndicatorViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class IndicatorViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
     """
     API endpoint to list and manage indicators of a given product.
     """
 
     serializer_class = serializers.IndicatorSerializer
     queryset = models.Indicator.objects.all()
-    permission_classes = [HasAPIKey | permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.HasValidAPIKeyOrReadOnly]
+    lookup_field = "id"
 
     def get_queryset(self, *args, **kwargs):
         queryset = super().get_queryset()
@@ -31,3 +39,17 @@ class IndicatorViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
             product = models.Product.objects.get(slug=product_slug)
             queryset = queryset.filter(productid=product)
         return queryset
+
+    def perform_create(self, serializer):
+        """Set the product ."""
+        product = models.Product.objects.filter(slug=self.kwargs["product_id"])
+        if product.exists():
+            serializer.validated_data["productid"] = product[0]
+
+            try:
+                indicator = super().perform_create(serializer)
+            except IntegrityError:
+                raise exceptions.ValidationError(
+                    message={"NON_FIELD_ERRORS": "Cannot create duplicate key"}
+                )
+            return indicator
--- django/core/tests/api/indicators/test_api_indicators.py
+++ django/core/tests/api/indicators/test_api_indicators.py
@@ -40,6 +40,7 @@ def test_api_indicators_list__anonymous_ok():
     ]
 
 
+# CREATE
 def test_api_indicators_create__anonymous_cannot_create():
     """Anonymous users should not be allowed to create indicators."""
     product = factories.ProductFactory()
@@ -49,4 +50,147 @@ def test_api_indicators_create__anonymous_cannot_create():
         body="{'nom_service_public_numerique': 'product'}",
     )
     assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert len(models.Product.objects.all()) == 1
+    assert not models.Indicator.objects.exists()
+
+
+def test_api_indicators_create__invalid_api_key_cannot_create():
+    """Calls bearing an invalid api key should not be able to create indicators."""
+    product = factories.ProductFactory()
+    api_key, key = models.ProductAPIKey.objects.create_key(
+        name="valid_key", product=product
+    )
+
+    response = APIClient().post(
+        f"/api/products/{product.slug}/indicators/",
+        {
+            "indicateur": "participants",
+            "valeur": 3,
+            "unite_mesure": "unite",
+            "frequence_monitoring": "mensuelle",
+            "date": "2025-06-30",
+            "date_debut": "2025-04-01",
+            "est_periode": "true",
+            "est_automatise": "false",
+        },
+        headers={"x-api-key": key + "ko"},
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert not models.Indicator.objects.exists()
+
+
+def test_api_indicators_create__valid_api_key_can_create():
+    """Calls bearing a working API key for this product can create indicator for product."""
+    product = factories.ProductFactory()
+    api_key, key = models.ProductAPIKey.objects.create_key(
+        name="valid_key", product=product
+    )
+
+    response = APIClient().post(
+        f"/api/products/{product.slug}/indicators/",
+        {
+            "indicateur": "participants",
+            "valeur": 3,
+            "unite_mesure": "unite",
+            "frequence_monitoring": "mensuelle",
+            "date": "2025-06-30",
+            "date_debut": "2025-04-01",
+            "est_periode": "true",
+            "est_automatise": "false",
+        },
+        headers={"x-api-key": key},
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+    assert models.Indicator.objects.exists()
+
+
+def test_api_indicators_create__cannot_create_duplicate():
+    """Should not be able to create duplicate."""
+    product = factories.ProductFactory()
+    api_key, key = models.ProductAPIKey.objects.create_key(
+        name="valid_key", product=product
+    )
+    data = {
+        "indicateur": "participants",
+        "valeur": 3,
+        "unite_mesure": "unite",
+        "frequence_monitoring": "mensuelle",
+        "date": "2025-06-30",
+        "date_debut": "2025-04-01",
+        "est_periode": True,
+        "est_automatise": False,
+    }
+
+    models.Indicator.objects.create(productid=product, **data)
+
+    response = APIClient().post(
+        f"/api/products/{product.slug}/indicators/",
+        data,
+        headers={"x-api-key": key},
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert len(models.Indicator.objects.all()) == 1
+
+
+# RETRIEVE
+def test_api_indicators_retrieve__anonymous_ok():
+    """Anonymous users should be allowed to delete indicators."""
+    indicator = factories.IndicatorFactory()
+
+    response = APIClient().get(
+        f"/api/products/{indicator.productid.slug}/indicators/{indicator.id}/",
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "id": str(indicator.id),
+        "indicateur": indicator.indicateur,
+        "valeur": indicator.valeur,
+        "unite_mesure": indicator.unite_mesure,
+        "frequence_monitoring": indicator.frequence_monitoring,
+        "date": str(indicator.date),
+        "date_debut": str(indicator.date_debut),
+        "est_periode": indicator.est_periode,
+        "est_automatise": indicator.est_automatise,
+        "productid": str(indicator.productid.id),
+    }
+
+
+# DELETE
+def test_api_indicators_delete__anonymous_cannot_delete():
+    """Anonymous users should not be allowed to delete indicators."""
+    indicator = factories.IndicatorFactory()
+
+    response = APIClient().delete(
+        f"/api/products/{indicator.productid.slug}/indicators/{indicator.id}/",
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert models.Indicator.objects.exists()
+
+
+def test_api_indicators_delete__invalid_api_key_cannot_delete():
+    """Calls bearing an invalid API key should not be able to delete indicators."""
+    indicator = factories.IndicatorFactory()
+    api_key, key = models.ProductAPIKey.objects.create_key(
+        name="valid_key", product=indicator.productid
+    )
+
+    response = APIClient().delete(
+        f"/api/products/{indicator.productid.slug}/indicators/{indicator.id}/",
+        headers={"x-api-key": key + "ko"},
+    )
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert models.Indicator.objects.exists()
+
+
+def test_api_indicators_delete__valid_api_key_can_delete():
+    """Calls bearing a valid API key for this product can delete indicator for product."""
+    indicator = factories.IndicatorFactory()
+    api_key, key = models.ProductAPIKey.objects.create_key(
+        name="valid_key", product=indicator.productid
+    )
+
+    response = APIClient().delete(
+        f"/api/products/{indicator.productid.slug}/indicators/{indicator.id}/",
+        headers={"x-api-key": key},
+    )
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert not models.Indicator.objects.exists()
