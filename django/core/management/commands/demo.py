89ca505edd76c390eb655789c02c0dafa6b8cbe7
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2025-08-04 15:26:23 +0200

    (demo) fix demo command

--- django/core/management/commands/demo.py
+++ django/core/management/commands/demo.py
@@ -2,22 +2,33 @@
 
 import random
 from core import models
+from django.core.management.base import BaseCommand
+
 
 services = ["service connect", "mon france pro"]
 
-for service in services:
-    product = models.Product.objects.get_or_create(nom_service_public_numerique=service)
-    for i in range(0, 5):
-        models.Indicator.objects.create(
-            productid=product[0],
-            indicateur=random.choice(
-                ["utilisateurs actifs", "indicateur2", "autre_indicateur"]
-            ),
-            valeur=random.randint(1, 300000),
-            unite_mesure="unite",
-            frequence_monitoring="mensuelle",
-            date=f"2025-{format(random.randint(1, 12), '02d')}-30",
-            est_periode=True,
-            est_automatise=random.choice([True, False]),
-            date_debut="",
-        )
+
+class Command(BaseCommand):
+    """
+    Management command populate local database and ease development
+    """
+
+    def handle(self, *args, **options):
+        for service in services:
+            product, is_created = models.Product.objects.get_or_create(
+                nom_service_public_numerique=service
+            )
+            for i in range(1, 13):
+                models.Indicator.objects.create(
+                    productid=product,
+                    indicateur=random.choice(
+                        ["utilisateurs actifs", "indicateur2", "autre_indicateur"]
+                    ),
+                    valeur=random.randint(1, 300000),
+                    unite_mesure="unite",
+                    frequence_monitoring="mensuelle",
+                    date=f"2024-{format(i, '02d')}-30",
+                    est_periode=True,
+                    est_automatise=random.choice([True, False]),
+                    date_debut="",
+                )
