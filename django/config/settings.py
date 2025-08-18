46846f88cc01f0d15a8428b2277c18e907a61416
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2025-08-18 16:22:16 +0200

    (api) change renderer to make API more browsable

--- django/config/settings.py
+++ django/config/settings.py
@@ -144,6 +144,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
     ],
 }
 
