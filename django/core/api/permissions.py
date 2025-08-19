b996632cb28c099441a5c3eb721daec3821ac453
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2025-08-19 19:18:11 +0200

    (settings) admin api key can create/delete indicators on all products

--- django/core/api/permissions.py
+++ django/core/api/permissions.py
@@ -2,7 +2,7 @@ from rest_framework import permissions
 from rest_framework import exceptions
 from core import models
 from django.shortcuts import get_object_or_404
-
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 
@@ -18,6 +18,9 @@ class HasValidAPIKeyOrReadOnly(permissions.BasePermission):
 
         product = get_object_or_404(models.Product, slug=view.kwargs["product_slug"])
         if key := request.headers.get("X-Api-Key"):
+            if request.headers.get("X-Api-Key") == settings.ADMIN_API_KEY:
+                return True
+
             try:
                 api_key = models.ProductAPIKey.objects.get_from_key(key)
             except models.ProductAPIKey.DoesNotExist:
