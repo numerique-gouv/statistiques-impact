f427e200454f27de53865ecbb3eeb7ba552fbe5e
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2025-08-29 18:04:35 +0200

    (adaptors) create messagerie adaptor

--- django/cron_tasks/management/commands/fetch_new_data.py
+++ django/cron_tasks/management/commands/fetch_new_data.py
@@ -2,6 +2,7 @@
 
 from django.core.management.base import BaseCommand
 from cron_tasks.adaptors.proconnect import ProConnectAdaptor
+from cron_tasks.adaptors.messagerie import MessagerieAdaptor
 
 
 class Command(BaseCommand):
@@ -12,3 +13,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         pc_adaptor = ProConnectAdaptor()
         pc_adaptor.fetch_latest_data()
+
+        messagerie_adaptor = MessagerieAdaptor()
+        messagerie_adaptor.fetch_latest_data()
---
+++ django/cron_tasks/tests/fixtures.py
@@ -0,0 +1,37 @@
+messagerie_response = (
+    {
+        "__id": 21,
+        "yyyy-mm-dd": "2025-06-28",
+        "utilisateurs uniques la veille": 392,
+        " sur les 7 derniers jours": 474,
+        " sur les 30 derniers jours": 572,
+    },
+    {
+        "__id": 22,
+        "yyyy-mm-dd": "2025-06-29",
+        "utilisateurs uniques la veille": 270,
+        " sur les 7 derniers jours": 475,
+        " sur les 30 derniers jours": 572,
+    },
+    {
+        "__id": 23,
+        "yyyy-mm-dd": "2025-06-30",
+        "utilisateurs uniques la veille": 268,
+        " sur les 7 derniers jours": 474,
+        " sur les 30 derniers jours": 572,
+    },
+    {
+        "__id": 24,
+        "yyyy-mm-dd": "2025-07-01",
+        "utilisateurs uniques la veille": 418,
+        " sur les 7 derniers jours": 482,
+        " sur les 30 derniers jours": 580,
+    },
+    {
+        "__id": 25,
+        "yyyy-mm-dd": "2025-07-02",
+        "utilisateurs uniques la veille": 428,
+        " sur les 7 derniers jours": 489,
+        " sur les 30 derniers jours": 589,
+    },
+)
