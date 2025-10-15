755a17be49ac8a40c565f75b9a15b39f0cadfe2a
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2025-10-15 15:39:22 +0200

    (products) improve last indicators table

--- django/core/views.py
+++ django/core/views.py
@@ -4,5 +4,18 @@ from core.models import Product
 
 
 def products(request: HttpRequest) -> HttpResponse:
-    products = Product.objects.all()
-    return render(request, "core/products.html", context={"products": products})
+    products_last_indicators = sorted(
+        [
+            {
+                "name": product.nom_service_public_numerique,
+                "last_indicators": product.last_indicators,
+            }
+            for product in Product.objects.all()
+            if product.last_indicators
+        ],
+        key=lambda x: x["last_indicators"][0].date,
+        reverse=True,
+    )
+    return render(
+        request, "core/products.html", context={"products": products_last_indicators}
+    )
