e3aa8f1fd07c448a9c3fe3b2559cfa714c84991c
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2025-11-13 17:59:55 +0100

    (cron) add daily cron to merge FT data

--- django/core/management/commands/merge_daily_FT_data.py
+++ django/core/management/commands/merge_daily_FT_data.py
@@ -2,9 +2,9 @@
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from core.utils import utils
 from core.utils.datagouv_client import DataGouvClient
 from core.adaptors import FranceTransfertAdaptor
+from datetime import date
 
 
 class Command(BaseCommand):
@@ -14,8 +14,21 @@ class Command(BaseCommand):
     receiving file but this would multiply the download / upload
     """
 
+    help = "Merge all daily files into monthly on France-transfert dataset"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "month",
+            nargs="?",
+            type=str,
+            default=str(date.today())[0:-3],
+            help="Month you want to merge files of. Default is current month.",
+        )
+
     def handle(self, *args, **options):
         """Call all adaptors to create indicators."""
+        # self.stdout.write(self.style.SUCCESS("..."))
+        month = options["month"]
         adaptor = FranceTransfertAdaptor()
         dataset_id = adaptor.product.dataset_id
         client = DataGouvClient()
@@ -25,6 +38,8 @@ class Command(BaseCommand):
             client.env = "demo"
             client.api_url = "https://demo.data.gouv.fr/api/1"
 
-        month = str(utils.get_last_month_limits()[0])[0:-3]
+        self.stdout.write(
+            f'Starting merge job for month {month} on dataset "{dataset_id}".'
+        )
         dataset = client.get_dataset(dataset_id)
         client.merge_monthly_stats(dataset, month)
