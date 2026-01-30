12a2b1b17883613eb125a167bb0696d17f647e8d
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2026-01-30 19:11:04 +0100

    (products) simple product pages

--- django/core/views.py
+++ django/core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from core.models import Product
 
 
@@ -19,3 +19,10 @@ def last_indicators(request: HttpRequest) -> HttpResponse:
     return render(
         request, "core/last_indicators.html", context={"products": last_indicators}
     )
+
+
+def product(request, product_slug) -> HttpResponse:
+    """Page for a single product"""
+
+    product = get_object_or_404(Product, slug=product_slug)
+    return render(request, "core/product.html", context={"product": product})
