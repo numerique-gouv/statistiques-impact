0882d296e68f69207e44585ec7c061c325b32a89
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2026-03-02 18:22:45 +0100

    (proxy) remove reverse proxy
    remove reverse proxy since express app
    has been deleted

--- django/config/urls.py
+++ django/config/urls.py
@@ -2,14 +2,11 @@
 URL configuration for stats projects
 """
 
-from django.urls import include, path, re_path
-from django.conf import settings
+from django.urls import include, path
 
 from core import urls as core_urls
 
-from revproxy.views import ProxyView
 
 urlpatterns = [
     path("", include(core_urls)),
-    re_path(r"(?P<path>.*)", ProxyView.as_view(upstream=settings.UPSTREAM_EXPRESS)),
 ]
