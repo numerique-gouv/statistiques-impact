c9ecb8db505f2a4473e4c5943cf87df254518b2f
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2026-07-02 12:00:35 +0200

    (dependencies) remove revproxy

--- django/config/settings.py
+++ django/config/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = (
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
-    "revproxy.apps.RevProxyConfig",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
