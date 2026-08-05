f20a32403b900453b06f5a7d33d3659270c9193d
Marie PUPO JEAMMET <marie.pupojeammet@numerique.gouv.fr>
2026-08-05 18:41:37 +0200

    (tests) add test for demo

---
+++ django/core/tests/test_demo.py
@@ -0,0 +1,16 @@
+import pytest
+from core import models
+from django.core.management import call_command
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+pytestmark = pytest.mark.django_db
+
+
+def test_demo():
+    call_command("demo")
+
+    assert User.objects.filter(is_staff = True).count() == 1
+    assert models.Product.objects.count() == 3
+    assert models.Indicator.objects.count() == 15
+    assert models.Record.objects.count() == 180
